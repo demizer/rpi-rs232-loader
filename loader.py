b03bff8913154b9a29779d6c3d5ee9bd5b6976eb
Jesus Alvarez <jeezusjr@gmail.com>
2014-03-31 12:19:55 -0600

    loader.py: Refactor
    * Add support for dbus signals
    * Refactor code into classes
    * Add support for glib event system

--- loader.py
+++ loader.py
@@ -1,44 +1,92 @@
-import RPIO
+import sys
 import time
 import datetime
 
+import RPIO
+
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GObject
+
+
 print("loader.py started")
 
-RPIO.setmode(RPIO.BOARD)
 
-RPIO.setup(11, RPIO.IN) # Pull down resistor connected to ground
-RPIO.setup(16, RPIO.OUT)
+class Drives:
+    def __init__(self):
+        pass
 
-# blinking function
-def blink(pin):
-        RPIO.output(pin, RPIO.HIGH)
-        time.sleep(0.1)
-        RPIO.output(pin, RPIO.LOW)
-        time.sleep(0.1)
-        # RPIO.output(pin, RPIO.HIGH)
-        return
+class DBusManager:
+    """A manager of DBus objects."""
 
-pushCounter = 0
-lastState = False
-buttonState = False
-lastEvent = datetime.datetime.now()
+    def __init__(self, event_loop):
+        self.bus = dbus.SystemBus(mainloop=event_loop)
+        self.dbus_obj = bus.get_object("org.freedesktop.UDisks2", "/org/freedesktop/UDisks2")
+        self.dbus_manager = dbus.Interface(obj, "org.freedesktop.DBus.ObjectManager")
+        # managed_objects = manager.GetManagedObjects()
+        self.dbus_manager.connect_to_signal("InterfacesAdded", self._on_interfaces_added)
+        self.dbus_manager.connect_to_signal("InterfacesRemoved", self._on_interfaces_removed)
+        self.drives = Drives()
 
-print("loader.py ready")
+    def _on_interfaces_added(self, object_path, interfaces_and_properties):
+        """TODO"""
+        print("Interface added!", object_path)
 
-try:
-    while True:
-        buttonState = RPIO.input(11)
-        if buttonState != lastState:
-            if buttonState:
-                pushCounter += 1
-                time.sleep(0.17)
-                lastEvent = datetime.datetime.now()
-        lastState = buttonState
-        if pushCounter > 0 and (datetime.datetime.now() - lastEvent).seconds > 1:
-            print("Button pressed", pushCounter, "times.")
-            pushCounter = 0
 
-except (KeyboardInterrupt, SystemExit):
-    print("Goodbye!")
-finally:
-    RPIO.cleanup()
+    def _on_interfaces_removed(self, object_path, interfaces):
+        """TODO"""
+        print("Interface removed!", object_path)
+
+
+class RpiIO:
+    """An object to manage input/output from the Raspberry Pi."""
+
+    def __init__(self):
+        pass
+        RPIO.setmode(RPIO.BOARD)
+
+        RPIO.setup(11, RPIO.IN) # Pull down resistor connected to ground
+        RPIO.setup(16, RPIO.OUT)
+
+        # blinking function
+        def blink(pin):
+                    RPIO.output(pin, RPIO.HIGH)
+                    time.sleep(0.1)
+                    RPIO.output(pin, RPIO.LOW)
+                    time.sleep(0.1)
+                    return
+
+        pushCounter = 0
+        lastState = False
+        buttonState = False
+        lastEvent = datetime.datetime.now()
+
+        try:
+                while True:
+                    buttonState = RPIO.input(11)
+                    if buttonState != lastState:
+                        if buttonState:
+                            pushCounter += 1
+                            time.sleep(0.17)
+                            lastEvent = datetime.datetime.now()
+                    lastState = buttonState
+                    if pushCounter > 0 and (datetime.datetime.now() - lastEvent).seconds > 1:
+                        print("Button pressed", pushCounter, "times.")
+                        pushCounter = 0
+        except (KeyboardInterrupt, SystemExit):
+                print("Goodbye!")
+        finally:
+                RPIO.cleanup()
+
+
+def main():
+    loop = GObject.MainLoop()
+    manager = DBusManager(loop)
+    try:
+        loop.run()
+    except KeyboardInterrupt:
+        loop.quit()
+
+
+if __name__ == "__main__":
+    main()
