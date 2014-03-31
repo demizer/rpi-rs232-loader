481f8f36222e645fa8f004e78c1bf7a69318be4c
Jesus Alvarez <jeezusjr@gmail.com>
2014-03-31 12:29:00 -0600

    loader.py: Make executable

--- loader.py
+++ loader.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import sys
 import time
 import datetime
@@ -43,40 +45,40 @@ class RpiIO:
 
     def __init__(self):
         pass
-        RPIO.setmode(RPIO.BOARD)
+        # RPIO.setmode(RPIO.BOARD)
 
-        RPIO.setup(11, RPIO.IN) # Pull down resistor connected to ground
-        RPIO.setup(16, RPIO.OUT)
+        # RPIO.setup(11, RPIO.IN) # Pull down resistor connected to ground
+        # RPIO.setup(16, RPIO.OUT)
 
-        # blinking function
-        def blink(pin):
-                    RPIO.output(pin, RPIO.HIGH)
-                    time.sleep(0.1)
-                    RPIO.output(pin, RPIO.LOW)
-                    time.sleep(0.1)
-                    return
+        # # blinking function
+        # def blink(pin):
+                    # RPIO.output(pin, RPIO.HIGH)
+                    # time.sleep(0.1)
+                    # RPIO.output(pin, RPIO.LOW)
+                    # time.sleep(0.1)
+                    # return
 
-        pushCounter = 0
-        lastState = False
-        buttonState = False
-        lastEvent = datetime.datetime.now()
+        # pushCounter = 0
+        # lastState = False
+        # buttonState = False
+        # lastEvent = datetime.datetime.now()
 
-        try:
-                while True:
-                    buttonState = RPIO.input(11)
-                    if buttonState != lastState:
-                        if buttonState:
-                            pushCounter += 1
-                            time.sleep(0.17)
-                            lastEvent = datetime.datetime.now()
-                    lastState = buttonState
-                    if pushCounter > 0 and (datetime.datetime.now() - lastEvent).seconds > 1:
-                        print("Button pressed", pushCounter, "times.")
-                        pushCounter = 0
-        except (KeyboardInterrupt, SystemExit):
-                print("Goodbye!")
-        finally:
-                RPIO.cleanup()
+        # try:
+                # while True:
+                    # buttonState = RPIO.input(11)
+                    # if buttonState != lastState:
+                        # if buttonState:
+                            # pushCounter += 1
+                            # time.sleep(0.17)
+                            # lastEvent = datetime.datetime.now()
+                    # lastState = buttonState
+                    # if pushCounter > 0 and (datetime.datetime.now() - lastEvent).seconds > 1:
+                        # print("Button pressed", pushCounter, "times.")
+                        # pushCounter = 0
+        # except (KeyboardInterrupt, SystemExit):
+                # print("Goodbye!")
+        # finally:
+                # RPIO.cleanup()
 
 
 def main():
